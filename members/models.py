f645e314f5ffdedf2141be4ead91ea3280d7a046
Behzod0317 <behzod0317@gmail.com>
2023-03-06 18:55:52 +0500

    first commit

---
+++ members/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Member(models.Model):
+    first_name = models.CharField(max_length=120)
+    last_name = models.CharField(max_length=120)
+    phone = models.IntegerField(null=True)
+    joined_data = models.DateField(null=True)
+    
+    slug = models.SlugField(default="", null=False)
+    
+    def __str__(self):
+
+        return f"{self.first_name} {self.last_name}"
